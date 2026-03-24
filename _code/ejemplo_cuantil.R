68c4ab0205028ce9379fc8715a890071cfb19eb8
alvarolimber <alvaro.rqsbo@gmail.com>
2026-03-23 23:59:27 -0400

    up

--- _code/ejemplo_cuantil.R
+++ _code/ejemplo_cuantil.R
@@ -14,6 +14,7 @@ library(labelled)
 library(ggplot2)
 library(GGally)
 library(quantreg)
+library(car)
 ###########################################
 load("_dataset/eh24.RData")
 bd<-eh24p %>% mutate(edad=as.numeric(s01a_03),
@@ -24,20 +25,8 @@ deciles<-seq(0.1,0.9,0.1)
 m3<-rq(log(ylab)~aestudio+female, data= bd, tau=deciles, method = "fn")
 summary(m2)
 summary(m3)
-plot(m2)
-
-ggplot(bd, aes(x = aestudio, y = log(ylab))) +
-  geom_point(alpha = 0.3) +
-  geom_quantile(quantiles = deciles, colour = "blue", alpha = 0.5) +
-  geom_smooth(method = "lm", se = FALSE, color = "red", linetype = "dashed") +
-  labs(title = "Regresión Cuantílica: Ingreso vs Educación",
-       subtitle = "Líneas azules: Deciles | Línea roja segmentada: MCO (Media)",
-       x = "Años de Educación",
-       y = "Ingreso Logarítmico") +
-  theme_minimal()
-
 print(anova(m3, joint = F))
-
 AIC(m3)
 AIC(m2)
 plot(m3)
+#VIF sobre lm

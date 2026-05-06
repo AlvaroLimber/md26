5d93c89c869c96f97b5367f77c598a06035bb251
alvarolimber <alvaro.rqsbo@gmail.com>
2026-05-06 11:38:33 -0400

    up

--- _code/ejemplo_asociaciones.R
+++ _code/ejemplo_asociaciones.R
@@ -1,32 +1,49 @@
+rm(list=ls())
+library(tidyverse)
 library(arulesViz)
 library(arules)
+#dataset
 data("Groceries")
 bdt <- Groceries
-
-# Ejecución de Apriori
-# supp: El ítem debe aparecer en al menos el 1% de las compras
-# conf: La regla debe cumplirse en el 50% de los casos
-# Generamos reglas
-reglas <- apriori(Groceries, parameter = list(supp = 0.005, conf = 0.5))
-
-# Filtramos las top 20 reglas por Lift para que el gráfico sea legible
-top_reglas <- sort(reglas, by = "lift")[1:20]
-plot(top_reglas, method = "graph", engine = "ggplot2")
-plot(top_reglas, method = "graph", engine = "htmlwidget")
-lot(ma1)
-plot(ma1, interactive=T)
-plot(ma3, method = "graph")
-plot(ma1, method = "matrix")
-plot(ma1, method = "grouped matrix")
-ruleExplorer(top_reglas)
-ruleExplorer(ma2)
+#Apriori
+reglas <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.5))
+reglas
+top20 <- sort(reglas, by = "lift")[1:20]
+plot(top20)
+plot(top20, interactive=T)
+plot(top20, method = "graph")
+plot(top20, method = "graph", engine = "htmlwidget")
+plot(top20, method = "matrix")
+plot(top20, method = "grouped matrix")
+ruleExplorer(top20)
+ruleExplorer(reglas)
 ruleExplorer(ma3)
-# Ordenar por Lift (fuerza de la asociación) y ver las top 5
-inspect(sort(reglas_apriori, by = "lift")[1:5])
-# Ejecución de Eclat
-# Solo busca conjuntos de al menos 2 productos que aparezcan en el 2% de las transacciones
-itemsets_eclat <- eclat(bdt, 
-                        parameter = list(supp = 0.02, minlen = 2))
+inspect(top20)
 
-# Ver los conjuntos más frecuentes
-inspect(sort(itemsets_eclat, by = "support")[1:5])
+soda_l=apriori(bdt, parameter=list(supp=0.001, 
+                                   conf =0.1, minlen=2), 
+               appearance = list(default="rhs",lhs="soda"))
+
+soda_r=apriori(bdt, parameter=list(supp=0.001, 
+                                   conf =0.1, minlen=2), 
+               appearance = list(default="lhs",rhs="soda"))
+soda_l
+soda_r
+soda_r<-soda_r[!is.redundant(soda_r)]
+ruleExplorer(soda_r)
+
+subset(reglas, lhs %in% c("soda", "yogurt"))
+subset(reglas, lhs %ain% c("soda", "sausage"))
+subset(reglas, rhs %in% c("sausage"))
+
+###ECLAT
+res_eclat <- eclat(bdt, 
+                   parameter = list(supp = 0.001, minlen = 2))
+res_eclat
+inspect(sort(res_eclat, by = "support")[1:5])
+plot(res_eclat)
+
+reglas_eclat<-ruleInduction(res_eclat, 
+                            transactions=bdt,
+                            confidence=0.5)
+reglas_eclat
